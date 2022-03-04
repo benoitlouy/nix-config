c99463359fc94ef400c74694556dd3450cb9e4a4
Benoit Louy <benoit.louy@fastmail.com>
2022-03-04 16:29:35 -0500

    add icon to chatty

--- overlays/chatty.nix
+++ overlays/chatty.nix
@@ -11,12 +11,16 @@ self: super:
       stripRoot = false;
     };
 
-    buildInputs = [
-      self.adoptopenjdk-hotspot-bin-8
-      self.packr-jar
-    ];
+    icon = super.fetchurl {
+      url = "https://github.com/chatty/chatty.github.io/raw/master/img/app_main_32.png";
+      hash = "sha256-HR7Y9gUTdrNi+tP8CGzPU4pC+Sz5sidVC0mq3E334wc=";
+    };
 
     buildPhase = ''
+      mkdir -p chatty.iconset
+      cp $icon chatty.iconset/icon_32x32.png
+      /usr/bin/iconutil --convert icns chatty.iconset
+
       ${self.adoptopenjdk-hotspot-bin-8}/bin/java -jar ${self.packr-jar}/lib/packr-all.jar \
        --platform mac \
        --jdk ${self.adoptopenjdk-hotspot-bin-8} \
@@ -25,7 +29,8 @@ self: super:
        --classpath Chatty.jar \
        --mainclass chatty.Chatty \
        --vmargs Xmx1G \
-       --output Chatty.app
+       --output Chatty.app \
+       --icon chatty.icns
     '';
 
     installPhase = ''
