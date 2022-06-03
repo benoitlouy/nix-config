0b1d0b03995fd59df2fbe5df983f5ab8f166ff0a
Benoit Louy <benoit.louy@fastmail.com>
2022-06-03 17:11:04 -0400

    update yabai to 4.0.1

--- overlays/yabai.nix
+++ overlays/yabai.nix
@@ -3,11 +3,11 @@ self: super:
 {
   yabai = super.yabai.overrideAttrs (
     old: rec {
-      version = "4.0.0";
+      version = "4.0.1";
 
       src = self.fetchzip {
         url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-        hash = "sha256-CBoRyxrleCKzgwZQamhwh3zkotxZCHrL3tslfktxluc=";
+        hash = "sha256-dQZw7Df69aSbcU9ot6f73H3VzZEgo2qpgUM6IJRxk60=";
       };
     }
   );
