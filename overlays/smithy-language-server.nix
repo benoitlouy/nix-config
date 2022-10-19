9060d628caa63261d32e10f3e80887063097dfc9
Benoit Louy <benoit.louy@fastmail.com>
2022-10-19 09:42:36 -0400

    reproducable smithy-language-server derivation

--- overlays/smithy-language-server.nix
+++ overlays/smithy-language-server.nix
@@ -4,21 +4,30 @@ self: super:
   smithy-language-server = super.stdenv.mkDerivation rec {
     pname = "smithy-language-server";
     version = "0.0.19";
-    src = ./.;
 
-    builldInputs = [
-      super.coursier
-    ];
+    deps = super.stdenv.mkDerivation {
+      name = "${pname}-deps-${version}";
+      buildCommand = ''
+        ${super.coursier}/bin/cs fetch \
+          --cache $(pwd) \
+          com.disneystreaming.smithy:${pname}:${version} > $TMPDIR/deps
+        mkdir -p $out/share/java
+        cp -r $(< $TMPDIR/deps) $out/share/java/
+      '';
+      outputHashMode = "recursive";
+      outputHashAlgo = "sha256";
+      outputHash = "sha256-X8U8VfOi2dpJAGtQzL/Fgfr+8mpCi6Oqvjk8VCsjTTE=";
+    };
 
-    buildPhase = ''
-      mkdir $TMPDIR/cs_cache
-      mkdir $TMPDIR/home
-      ${super.coursier}/bin/cs bootstrap -J-Duser.home=$TMPDIR/home --cache $TMPDIR/cs_cache --standalone com.disneystreaming.smithy:${pname}:${version} -o ${pname}
-    '';
+    nativeBuildInputs = [ super.makeWrapper super.setJavaClassPath ];
+    buildInputs = [ deps ];
+
+    dontUnpack = true;
 
     installPhase = ''
       mkdir -p $out/bin
-      cp ${pname} $out/bin
+      makeWrapper ${super.jre}/bin/java $out/bin/${pname} \
+        --add-flags "-cp $CLASSPATH software.amazon.smithy.lsp.Main"
     '';
   };
 }
