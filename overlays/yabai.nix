c167375b9c6e3a1a037866bf169f668d360b1217
Benoit Louy <benoit.louy@fastmail.com>
2022-11-07 22:06:17 -0500

    update yabai overlay (#7)

--- overlays/yabai.nix
+++ overlays/yabai.nix
@@ -1,14 +1,29 @@
 self: super:
 
 {
-  yabai = super.yabai.overrideAttrs (
-    old: rec {
-      version = "5.0.1";
+  yabai = super.stdenv.mkDerivation rec {
+    pname = "yabai";
+    version = "5.0.1";
 
-      src = self.fetchzip {
-        url = "https://github.com//koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-        hash = "sha256-iCx/e3IwJ6YzgEy7wGkNQU/d7gaZd4b/RLwRvRpwVwQ=";
-      };
-    }
-  );
+    src = super.fetchzip {
+      url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
+      hash = "sha256-iCx/e3IwJ6YzgEy7wGkNQU/d7gaZd4b/RLwRvRpwVwQ=";
+    };
+
+    nativeBuildInputs = [
+      super.installShellFiles
+    ];
+
+    dontConfigure = true;
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp -r ./bin $out
+      installManPage ./doc/yabai.1
+      runHook postInstall
+    '';
+
+  };
 }
