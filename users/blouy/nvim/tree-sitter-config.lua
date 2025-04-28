b34b2587717b12451b0c818e329d587bccc78da6
Benoit Louy <benoit.louy@fastmail.com>
2025-04-28 10:21:38 -0400

    disable pylsp

--- users/blouy/nvim/tree-sitter-config.lua
+++ users/blouy/nvim/tree-sitter-config.lua
@@ -130,58 +130,60 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 
 require'lspconfig'.nixd.setup{}
 
-require'lspconfig'.pylsp.setup{
-  on_attach = on_attach,
-  settings = {
-    pylsp = {
-      plugins = {
-        -- black = {
-        --   enabled=true,
-        -- },
-        flake8 = {
-          enabled=true,
-          -- pyright overlap
-          ignore = {'F811', 'F401', 'F821', 'F841', 'E501', 'W503'},
-        },
-        pycodestyle = {
-          enabled=true,
-        },
-        autopep8 = {
-          enabled=false,
-        },
-        yapf = {
-          enabled=true,
-        },
-      },
-    },
-  },
-}
+-- require'lspconfig'.pylsp.setup{
+--   on_attach = on_attach,
+--   settings = {
+--     pylsp = {
+--       plugins = {
+--         -- black = {
+--         --   enabled=true,
+--         -- },
+--         flake8 = {
+--           enabled=true,
+--           -- pyright overlap
+--           ignore = {'F811', 'F401', 'F821', 'F841', 'E501', 'W503'},
+--         },
+--         pycodestyle = {
+--           enabled=true,
+--         },
+--         autopep8 = {
+--           enabled=false,
+--         },
+--         yapf = {
+--           enabled=true,
+--         },
+--       },
+--     },
+--   },
+-- }
 
 require'lspconfig'.pyright.setup{
   on_attach = on_attach,
   settings = {
     python = {
       analysis = {
-        typeCheckingMode = 'basic',
-        diagnosticSeverityOverrides = {
-          reportConstantRedefinition = 'warning',
-          reportDuplicateImport = 'warning',
-          reportMissingSuperCall = 'warning',
-          reportUnnecessaryCast = 'warning',
-          reportUnnecessaryComparison = 'warning',
-          reportUnnecessaryContains = 'warning',
-          reportCallInDefaultInitializer = 'info',
-          reportFunctionMemberAccess = 'info',
-          reportImportCycles = 'info',
-          reportMatchNotExhaustive = 'info',
-          reportShadowedImports = 'info',
-          reportUninitializedInstanceVariable = 'info',
-          reportUnnecessaryIsInstance = 'info',
-          reportUnusedClass = 'info',
-          reportUnusedFunction = 'info',
-          reportUnusedImport = 'info',
-          reportUnusedVariable = 'info',
-        },
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        -- typeCheckingMode = 'basic',
+        -- diagnosticSeverityOverrides = {
+        --   reportConstantRedefinition = 'warning',
+        --   reportDuplicateImport = 'warning',
+        --   reportMissingSuperCall = 'warning',
+        --   reportUnnecessaryCast = 'warning',
+        --   reportUnnecessaryComparison = 'warning',
+        --   reportUnnecessaryContains = 'warning',
+        --   reportCallInDefaultInitializer = 'info',
+        --   reportFunctionMemberAccess = 'info',
+        --   reportImportCycles = 'info',
+        --   reportMatchNotExhaustive = 'info',
+        --   reportShadowedImports = 'info',
+        --   reportUninitializedInstanceVariable = 'info',
+        --   reportUnnecessaryIsInstance = 'info',
+        --   reportUnusedClass = 'info',
+        --   reportUnusedFunction = 'info',
+        --   reportUnusedImport = 'info',
+        --   reportUnusedVariable = 'info',
+        -- },
       },
     },
   },
