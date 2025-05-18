f51fa421953963571cd56c576271212096a7b12f
Benoit Louy <benoit.louy@fastmail.com>
2025-05-17 22:22:51 -0400

    cleanup

--- overlays/cider.nix
+++ overlays/cider.nix
@@ -6,10 +6,6 @@ self: super:
     version = "2.0.3";
 
     src = ./cider-v2.0.3-linux-x64.AppImage;
-    # src = self.fetchurl {
-    #   url = "https://cdn.discordapp.com/attachments/1092593196862808084/1274060002990100632/Cider-1.0.0.AppImage?ex=66cc1517&is=66cac397&hm=1775c4068b330ed8dcb4757450c692b6ea4fd56ac4915819a60381c758af92b4&";
-    #   hash = "sha256-rTSFaZK5incYxqUHlkdSMmCK4kK3N72qRZUSpDK3rqg=";
-    # };
 
     extraInstallCommands =
       let contents = self.appimageTools.extract { inherit pname version src; };
