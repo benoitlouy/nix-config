23968a12634e2bb0b36dea46c7bd80bba019326f
Benoit Louy <benoit.louy@fastmail.com>
2025-05-26 23:38:21 -0400

    update flake

--- hosts/L/configuration.nix
+++ hosts/L/configuration.nix
@@ -250,10 +250,10 @@
       PLATFORM_PROFILE_ON_AC = "balanced";
       PLATFORM_PROFILE_ON_BAT = "low-power";
 
-      START_CHARGE_THRESH_BAT0 = 75;
-      STOP_CHARGE_THRESH_BAT0 = 80;
+      START_CHARGE_THRESH_BAT0 = 95;
+      STOP_CHARGE_THRESH_BAT0 = 100;
 
-      CPU_BOOST_ON_AC = 0;
+      CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
 
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
