7471f85311dc31aad6b1bcacda58d757c551b3ff
Benoit Louy <benoit.louy@fastmail.com>
2025-08-05 09:39:29 -0400

    use default score for copilot completions

--- users/blouy/nvim/default.nix
+++ users/blouy/nvim/default.nix
@@ -62,7 +62,6 @@ let
               copilot = {
                 name = "copilot",
                 module = "blink-copilot",
-                score_offset = 100,
                 async = true,
                 opts = {
                   max_completions = 3,
