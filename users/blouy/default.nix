5848fdd1f86eb0410a43c4b010ee3ed5111b0a30
Benoit Louy <benoit.louy@fastmail.com>
2025-10-12 18:27:58 -0400

    fastfetch

--- users/blouy/default.nix
+++ users/blouy/default.nix
@@ -414,6 +414,129 @@ in
     enable = true;
   };
 
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      general = {
+        preRun = "clear; echo '\n\n'";
+      };
+      logo = {
+        padding = {
+          top = 2;
+          left = 1;
+          right = 2;
+        };
+      };
+      display = {
+        separator = "  ";
+      };
+      modules = [
+        # Title
+        {
+          type = "title";
+          format = "{#1}╭────────────────────────────────╮";
+          # format = "{#1}╭───────────── {#}{user-name-colored}";
+        }
+        # System Information
+        {
+          type = "custom";
+          format = "{#1}│ {#}System Information";
+        }
+        {
+          type = "os";
+          key = "{#separator}│  {#keys}󰍹 OS";
+        }
+        {
+          type = "kernel";
+          key = "{#separator}│  {#keys}󰒋 Kernel";
+        }
+        {
+          type = "uptime";
+          key = "{#separator}│  {#keys}󰅐 Uptime";
+        }
+        {
+          type = "packages";
+          key = "{#separator}│  {#keys}󰏖 Packages";
+          format = "{all}";
+        }
+        {
+          type = "custom";
+          format = "{#1}│";
+        }
+        # Desktop Environment
+        {
+          type = "custom";
+          format = "{#1}│ {#}Desktop Environment";
+        }
+        {
+          type = "de";
+          key = "{#separator}│  {#keys}󰧨 DE";
+        }
+        {
+          type = "wm";
+          key = "{#separator}│  {#keys}󱂬 WM";
+        }
+        {
+          type = "wmtheme";
+          key = "{#separator}│  {#keys}󰉼 Theme";
+        }
+        {
+          type = "display";
+          key = "{#separator}│  {#keys}󰹑 Resolution";
+        }
+        {
+          type = "shell";
+          key = "{#separator}│  {#keys}󰞷 Shell";
+        }
+        {
+          type = "terminalfont";
+          key = "{#separator}│  {#keys}󰛖 Font";
+        }
+        {
+          type = "custom";
+          format = "{#1}│";
+        }
+        # Hardware Information
+        {
+          type = "custom";
+          format = "{#1}│ {#}Hardware Information";
+        }
+        {
+          type = "cpu";
+          key = "{#separator}│  {#keys}󰻠 CPU";
+        }
+        {
+          type = "gpu";
+          key = "{#separator}│  {#keys}󰢮 GPU";
+        }
+        {
+          type = "memory";
+          key = "{#separator}│  {#keys}󰍛 Memory";
+        }
+        {
+          type = "disk";
+          key = "{#separator}│  {#keys}󰋊 Disk (/)";
+          folders = "/";
+        }
+        {
+          type = "custom";
+          format = "{#1}│";
+        }
+        # Colors
+        {
+          type = "colors";
+          key = "{#separator}│";
+          symbol = "circle";
+        }
+        # Footer
+        {
+          type = "custom";
+          format = "{#1}╰───────────────────────────────╯";
+        }
+      ];
+    };
+  };
+
   xdg.mimeApps = let
     value = let
       zen-browser = config.programs.zen-browser.package;
@@ -449,7 +572,8 @@ in
 
   xdg.configFile = {
     "ghostty/config".text = ''
-      theme = Oxocarbon
+      # theme = Oxocarbon
+      theme = Atom One Dark
       background-opacity = 0.8
       background-blur = true
       window-decoration = none
