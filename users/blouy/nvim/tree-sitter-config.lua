81d03a7d5ff26795188b2b69b627b71d00a4bde5
Benoit Louy <benoit.louy@fastmail.com>
2026-01-06 19:01:42 -0500

    fix nvim-treesitter config

--- users/blouy/nvim/tree-sitter-config.lua
+++ users/blouy/nvim/tree-sitter-config.lua
@@ -2,100 +2,16 @@
 -- enable rainbow parenthesis while using tree sitter for syntax highlighting
 vim.api.nvim_set_hl(0, "@punctuation.bracket", { link = "" })
 
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.smithy" },
-  callback = function() vim.cmd("setfiletype smithy") end
+require 'nvim-treesitter'.setup {
+  install_dir = vim.fn.stdpath('data') .. '/site'
+}
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'smithy', 'scala', 'nix','lua', 'python', 'terraform' },
+  callback = function()
+    vim.treesitter.start()
+  end,
 })
-
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.smithy = {
-  filetype = "smithy" -- if filetype does not agrees with parser name
-}
-
-require 'nvim-treesitter.configs'.setup {
-  -- parser_install_dir = "~/.local/share/nvim/site",
-  -- A list of parser names, or "all"
-  -- ensure_installed = { "smithy" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- List of parsers to ignore installing (for "all")
-  -- ignore_install = { "javascript" },
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- disable = { "c", "rust" },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-
-  -- textobjects = {
-  --   select = {
-  --     enable = true,
-  --     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-  --     keymaps = {
-  --       -- You can use the capture groups defined in textobjects.scm
-  --       ['aa'] = '@parameter.outer',
-  --       ['ia'] = '@parameter.inner',
-  --       ['af'] = '@function.outer',
-  --       ['if'] = '@function.inner',
-  --       ['ac'] = '@class.outer',
-  --       ['ic'] = '@class.inner',
-  --       ['ii'] = '@conditional.inner',
-  --       ['ai'] = '@conditional.outer',
-  --       ['il'] = '@loop.inner',
-  --       ['al'] = '@loop.outer',
-  --       ['at'] = '@comment.outer',
-  --     },
-  --   },
-  --   move = {
-  --     enable = true,
-  --     set_jumps = true, -- whether to set jumps in the jumplist
-  --     goto_next_start = {
-  --       ['gl'] = '@function.outer',
-  --       ['gy'] = '@class.outer',
-  --     },
-  --     goto_next_end = {
-  --       ['gL'] = '@function.outer',
-  --       ['gY'] = '@class.outer',
-  --     },
-  --     goto_previous_start = {
-  --       ['gj'] = '@function.outer',
-  --       ['gu'] = '@class.outer',
-  --     },
-  --     goto_previous_end = {
-  --       ['gJ'] = '@function.outer',
-  --       ['gU'] = '@class.outer',
-  --     },
-  --     -- goto_next = {
-  --     --   [']i'] = "@conditional.inner",
-  --     -- },
-  --     -- goto_previous = {
-  --     --   ['[i'] = "@conditional.inner",
-  --     -- }
-  --   },
-  --   swap = {
-  --     enable = true,
-  --     swap_next = {
-  --       ['<leader>a'] = '@parameter.inner',
-  --     },
-  --     swap_previous = {
-  --       ['<leader>A'] = '@parameter.inner',
-  --     },
-  --   },
-  -- },
-}
 
 vim.opt.runtimepath:append("~/.config/nvim/site")
 
