48260993df405972074111b676971197476b5300
Benoit Louy <benoit.louy@fastmail.com>
2026-04-12 22:14:29 +0000

    configure ssh agent socket

--- modules/home-manager/services/ssh-agent/default.nix
+++ modules/home-manager/services/ssh-agent/default.nix
@@ -1,6 +1,8 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
   services.ssh-agent = {
     enable = true;
   };
+
+  home.sessionVariables.SSH_AUTH_SOCK = pkgs.lib.mkIf config.services.ssh-agent.enable "$XDG_RUNTIME_DIR/ssh-agent";
 }
